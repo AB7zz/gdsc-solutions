6bb5ee7024d8da212dc06a296bc457fb6a9280b4
AB7zz <abhinavcv007@gmail.com>
2024-01-16 01:37:22 +0530

    emptied fb

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -11,13 +11,13 @@ import emailjs from '@emailjs/browser';
 emailjs.init('Hb_PKHYL7lZDRKHv9')
 
 const firebaseConfig = {
-  apiKey:  import.meta.env.VITE_apiKey,
-  authDomain:  import.meta.env.VITE_authDomain,
-  projectId:  import.meta.env.VITE_projectId,
-  storageBucket:  import.meta.env.VITE_storageBucket,
-  messagingSenderId:  import.meta.env.VITE_messagingSenderId,
-  appId:  import.meta.env.VITE_appId,
-  measurementId:  import.meta.env.VITE_measurementId
+  // apiKey:  import.meta.env.VITE_apiKey,
+  // authDomain:  import.meta.env.VITE_authDomain,
+  // projectId:  import.meta.env.VITE_projectId,
+  // storageBucket:  import.meta.env.VITE_storageBucket,
+  // messagingSenderId:  import.meta.env.VITE_messagingSenderId,
+  // appId:  import.meta.env.VITE_appId,
+  // measurementId:  import.meta.env.VITE_measurementId
 };
 
 // Initialize Firebase
